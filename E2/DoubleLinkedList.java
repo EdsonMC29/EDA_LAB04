4a6c6c5c7d6a84a7796bc6b2adf4a080fb0a6fb9
Jorge Luis Escobedo Ocaña <jescobedooc@unsa.edu.pe>
2023-06-17 13:06:32 -0500

    segundo avance Ejercicio 2

--- E2/DoubleLinkedList.java
+++ E2/DoubleLinkedList.java
@@ -1,78 +1,21 @@
-import org.w3c.dom.Node;
+class DoubleLinkedList {
+    Node<Integer> ptr;
 
-public class DoubleLinkedList {
-
-    static Node ptr;
-    
-    public DoubleLinkedList (E A){
-        ptr = Node(A);
+    public DoubleLinkedList() {
+        ptr = null;
     }
 
     void addElem(int elem) {
-        Node nuevo = new Node(); // Crear nuevo nodo
-
-        if (ptr == null) { // Si la lista esta vacía
-            nuevo.data = elem;
+        Node<Integer> nuevo = new Node<>(elem);
+        if (ptr == null) {
             ptr = nuevo;
         } else {
-          // Si la lista no está vacía la recorremos hasta llegar al final, para agregar el nuevo elemento de último.
-            Node ant = ptr;
-            Node p = ptr.RLink;
-
-            while (p != null) {
-                ant = p;
-                p = p.RLink;
-            }		
-          /**
-          * null <-|n|=|ant|-> null
-          * =|ant|<-|nuevo|
-          * -|nuevo|-> null
-          * =|ant|=|nuevo|->null
-          */
-            if (p == null) {
-                nuevo.num = elem;
-                nuevo.LLink = ant;
-                nuevo.RLink = null;
-                ant.RLink = nuevo;
+            Node<Integer> p = ptr;
+            while (p.nextNode != null) {
+                p = p.nextNode;
             }
+            p.nextNode = nuevo;
+            nuevo.previousNode = p;
         }
-    }
-
-    public Node<T> get(int indice) {
-        Node<T> aux=raiz;
-        for(int i=0;i<indice;i++)
-            aux=aux.getNextNode();
-        return aux;
-    }
-
-    /**
-     * Mostrar en consola los elementos de la lista.
-     */
-    void mostrarLista() {
-        if (ptr == null) {
-            System.out.println("No hay elementos en la lista");
-        } else {
-            Nodo p = ptr;
-            while (p != null) {
-                System.out.println(p.num);
-            }
-            p = p.RLink;
-        }
-    }
-    
-    /**
-     * Buscar un elemento en la lista.
-     * @param elem Elemento que sera buscado.
-     * @return Falso si no fue encontrado. Verdadero si fue encontrado.
-     */
-    boolean buscarElem(int elem) {
-        Node temp = ptr;
-        while (temp != null) {
-            if (temp.num == elem)
-                return true;
-            temp = temp.RLink;
-        }
-        
-        return false;
     }
 }
--- E2/Prueba.java
+++ E2/Prueba.java
@@ -1,44 +1,63 @@
-    import java.util.*;
-    public class Prueba{
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
 
-        public static void main(String[] args) {		
-            int tamano;
-            
-            Scanner teclado = new Scanner( System.in );
-            System.out.print( "Introduzca el tamaño del arreglo: " );
-            tamano = teclado.nextInt();
-            
-            int arreglo[] = generarPeorCaso(tamano);
-            imprimirArreglo(arreglo);
-            insertionSort(arreglo);
+public class Prueba {
+
+    public static void main(String[] args) throws IOException {
+        int tamano;
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Introduzca el tamaño máximo del último arreglo: ");
+        tamano = teclado.nextInt();
+
+        ArrayList<DoubleLinkedList> casos = new ArrayList<DoubleLinkedList>();
+        String archivoInsercion = "insercion.txt";
+        PrintWriter oS = new PrintWriter(archivoInsercion);
+
+        for (int n = 1; n <= tamano; n++) {
+            casos.add(generarPeorCaso(n));
         }
 
-        public static DoubleLinkedList generarPeorCaso(int t) {
-            DoubleLinkedList lista = new DoubleLinkedList();
-            for (int i=0; i<t; i++) {
-                		
-            }
-            return lista;		
+        Iterator<DoubleLinkedList> puntero = casos.iterator();
+        while (puntero.hasNext()) {
+            DoubleLinkedList lista = puntero.next();
+            long tiempo = insertionSort(lista);
+            oS.println(String.valueOf(tiempo));
         }
 
-        public static long insertionSort(int[] A) {
-        int key;
-        int i;
-        long nano_startTime = System.nanoTime(); 
-        for(int j=1; j<A.length; j=j+1) {
-            key = A[j];
-            //Insertar A[j] en la secuencia ordenada A[1..j-1]
-            i = j-1;
-            while(i>-1 && A[i]>key) {
-                A[i+1] = A[i];
-                i = i-1;
-            }
-            A[i+1] = key;
-            //imprimirArreglo(A);
-        }
-        long nano_endTime = System.nanoTime();		
-        return nano_endTime - nano_startTime;		
-        }
+        oS.close();
 
-
+        // plot "/path/to/insercion.txt" with lines
+        JavaPlot p = new JavaPlot();
+        p.addPlot("\"" + archivoInsercion + "\" with lines");
+        p.plot();
     }
+
+    public static DoubleLinkedList generarPeorCaso(int t) {
+        DoubleLinkedList lista = new DoubleLinkedList();
+        for (int i = t; i > 0; i--) {
+            lista.addElem(i);
+        }
+        return lista;
+    }
+
+    public static long insertionSort(DoubleLinkedList lista) {
+        Node<Integer> ptr = lista.ptr;
+        long nano_startTime = System.nanoTime();
+        while (ptr != null) {
+            int key = ptr.data;
+            Node<Integer> current = ptr;
+            Node<Integer> prev = ptr.previousNode;
+            while (prev != null && prev.data > key) {
+                current.data = prev.data;
+                current = prev;
+                prev = prev.previousNode;
+            }
+            current.data = key;
+            ptr = ptr.nextNode;
+        }
+        long nano_endTime = System.nanoTime();
+        return nano_endTime - nano_startTime;
+    }
+}
